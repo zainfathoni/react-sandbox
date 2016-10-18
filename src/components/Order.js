b9816165bbadc19b5752d2bcf4c76be056ecec03
Zain Fathoni <zain.fathoni@gmail.com>
2016-10-18 16:03:53 +0700

    add: react-loading in Order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { formatPrice } from '../helpers';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
+import Loading from 'react-loading';
+
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
   renderOrder = (key) => {
@@ -31,6 +33,15 @@ class Order extends React.Component {
   }
 
   render() {
+    // Render Loading Bars
+    if (Object.keys(this.props.fishes).length === 0) {
+      return (
+        <div className="store-list">
+          <Loading type='bars' color='#000' />
+        </div>
+      )
+    }
+
     const orderIds = Object.keys(this.props.order);
     const total = orderIds.reduce((prevTotal, key) => {
       const fish = this.props.fishes[key];
@@ -41,6 +52,7 @@ class Order extends React.Component {
       }
       return prevTotal;
     }, 0 )
+
     return (
       <div className="order-wrap">
         <h2>Your Order</h2>
